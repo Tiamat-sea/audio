b9959da9e8ef22da4938b914fad730e9d9fa062a
Tiamat-sea <1666437230@qq.com>
2023-12-30 11:35:56 +0800

    引入wavesurfer

--- audio_analysis/src/router/index.js
+++ audio_analysis/src/router/index.js
@@ -5,12 +5,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: HomeView
     },
     {
-      path: '/about',
+      path: '/',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
---
+++ audio_analysis/src/views/Analysis.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script setup>
+import WaveSurfer from 'wavesurfer.js'
+
+const wavesurfer = WaveSurfer.create({
+  container: document.body,
+  waveColor: 'rgb(200, 0, 200)',
+  progressColor: 'rgb(100, 0, 100)',
+  url: '/examples/audio/audio.wav',
+})
+
+wavesurfer.on('click', () => {
+  wavesurfer.play()
+})
+</script>
